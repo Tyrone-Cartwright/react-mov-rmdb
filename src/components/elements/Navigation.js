e7bf745ab87a2b53985d46f040cdbf83a7ecf930
Tyrone Cartwright <tyrone.cartwright@yahoo.com>
2020-01-08 14:05:40 -0500

    fetched data and built out Navigation, MovieInfo, MovieInfoBar and Actor components and also prop-checked components

--- src/components/elements/Navigation.js
+++ src/components/elements/Navigation.js
@@ -1,5 +1,23 @@
 import React from 'react';
+import PropTypes from 'prop-types'
+import { Link } from '@reach/router'
 
-const Navigation = () => <div>Navigation</div>
+import { StyledNavigation } from '../styles/StyledNavigation'
+
+const Navigation = ({ movie }) => (
+  <StyledNavigation>
+    <div className='navigation-content'>
+      <Link to='/'>
+        <p>Home</p>
+      </Link>
+      <p>|</p>
+      <p>{movie}</p>
+    </div>
+  </StyledNavigation>
+)
+
+Navigation.propTypes = {
+  movie: PropTypes.string,
+}
 
 export default Navigation
