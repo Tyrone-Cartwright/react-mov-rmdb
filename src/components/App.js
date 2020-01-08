276bbcb94e28bbb199996ec7904787ff1d4fdb25
Tyrone Cartwright <tyrone.cartwright@yahoo.com>
2020-01-07 22:52:35 -0500

    scaffolded out Movie page with Actor, MovieInfo, MovieInfoBar, Navigation components

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,11 @@
 import React from 'react'
+import { Router } from '@reach/router'
 import { GlobalStyle } from './styles/GlobalStyle'
 
 import Header from './elements/Header'
 import Home from './Home'
+import Movie from './Movie'
+import NotFound from './NotFound'
 
 
 
@@ -10,7 +13,11 @@ const App = () =>
 (
   <>
     <Header />
-    <Home />
+    <Router>
+      <Home path='/' />
+      <Movie path='/:movieId' />
+      <NotFound default />    
+    </Router>
     <GlobalStyle />
   </>
 )
